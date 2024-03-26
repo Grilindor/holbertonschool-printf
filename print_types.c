ee002079fa4b37982e0355fe57c4f14967a765d4
Grilindor <marcor91@hotmail.fr>
2024-03-26 12:49:14 +0000

    to finish

--- print_types.c
+++ print_types.c
@@ -8,36 +8,27 @@
 
 int print_string(va_list chec_type)
 {
-	char *str = va_arg(chec_type, int);
+	char *str = va_arg(chec_type, char*);
 	int i;
 
 	for (i = 0; str[i] != '\0'; i++)
 		_putchar(str[i]);
-	return (str);
+	return (i);
 }
 
 int print_1_c(va_list chec_type)
 {
-	_putchar(va_arg(chec_type, int));
+	_putchar(va_arg(chec_type, char));
 	return (1);
 }
 
-int print_int(va_list chec_type)
-{
-	int *i = va_arg(chec_type, int);
-	int j;
-	for (j = 0; i[j] != '\0'; j++)
-		_putchar(i[j]);
-	return (i);
-}
 
 /**
 *Decimal , les chiffre en entier
 **/
 
-Int print_d(va_list chec_type)
+Int print_d_i(va_list chec_type)
 {
-	 *n = va_arg(chec_type, int);
 	
 }
 
@@ -47,5 +38,7 @@ Int print_d(va_list chec_type)
 
 int print_modulo(va_list chec_type)
 {
-	return (puts("%"));
+	(void)chec_type;
+	_putchar('%');
+	return (1);
 }
