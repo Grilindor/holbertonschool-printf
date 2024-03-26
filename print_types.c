162f3049305809e63179ed57fcbbb9a0c1eaa216
Grilindor <marcor91@hotmail.fr>
2024-03-26 14:59:35 +0000

    need to test it

--- print_types.c
+++ print_types.c
@@ -27,9 +27,33 @@ int print_1_c(va_list chec_type)
 *Decimal , les chiffre en entier
 **/
 
-Int print_d_i(va_list chec_type)
+int print_d_i(va_list chec_type)
 {
-	
+	int n = va_arg(chec_type, int);
+	int count = 0;
+	int long[12];
+	int index = 0, i;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+		count++;
+	}
+
+	do {
+		long[count] = n % 10;
+		n /= 10;
+		count++;
+	} while (n > 0);
+
+	for (i = index - 1; i >= 0; i--)
+	{
+		putchar('0' + digits[i]);
+		count++;
+	}
+
+	return (count);
 }
 
 /**
