3feb54590cf2d5ff9ab99cb97e0a3c111c62d4ec
Francianeny <nirinasoabozy@yahoo.fr>
2024-03-28 09:28:12 +0000

    printf return an integer

--- _printf.c
+++ _printf.c
@@ -3,9 +3,9 @@
 #include <stdarg.h>
 #include <stdio.h>
 /**
- *
- **/
-
+*@char: this is the type of the format argument
+*@format: the name of the arg that contains the formatting of the str to be printed.
+**/
 int _printf(const char *format, ...)
 {
 	array check[] = {
@@ -18,23 +18,28 @@ int _printf(const char *format, ...)
 	};
 
 	va_list chec_type;
-	int i = 0, j = 0, count = 0;
+	int i = 0, j = 0, count = 0; /* declaration of three integer variables*/
 
-	va_start(chec_type, format);
-
-	/*if (!format)
+	va_start(chec_type, format); /* va_start initializes va_list*/
+/**
+ *if (!format)
 	{
 		return(NULL);
-	}*/
-
-	while (format[i] && format)
+}
+**/
+	while (format[i] && format) /**checks that the format s-tring exists and
+				      *has not been terminated**/
 	{
 		if (format[i] == '%')
 		{
 			j = 0;
-			while(j < 5)
+			while (j < 5)
 			{
 				if (format[i + 1] == check[j].the_format_in_char[0])
+/**
+*it checks to see character following the %( format[i + 1]) = the first
+*character in the checkstructure.
+**/
 				{
 					count += check[j].funcp(chec_type);
 				}
@@ -44,11 +49,12 @@ int _printf(const char *format, ...)
 		}
 		else
 		{
-			_putchar(format[i]);
+			_putchar(format[i]);/**only be executed if preceding
+					      *condition is true**/
 			count++;
 		}
 		i++;
-        }
-        va_end(chec_type);
-        return (count);
+	}
+	va_end(chec_type);  /*va_end clean up the va_list*/
+	return (count);
 }
