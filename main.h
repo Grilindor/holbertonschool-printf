c94d55308ab6dd67b93c5d9929367ed55fc618aa
Francianeny <nirinasoabozy@yahoo.fr>
2024-03-28 10:09:06 +0000

    mh printf

--- main.h
+++ main.h
@@ -1,14 +1,15 @@
-#ifndef print_type.h
-#define print_type.h
-void print_string(va_list chec_type);
-void print_1_c(va_list chec_type);
-void print_int(va_list chec_type);
-void print_d(va_list chec_type);
-nt _printf(const char *format, ...);
+#ifndef PRINT_TYPE_H
+#define PRINT_TYPE_H
+#include <stdarg.h>
+int print_string(va_list chec_type);
+int print_1_c(va_list chec_type);
+int print_d_i(va_list chec_type);
+int print_modulo(va_list chec_type);
+int _printf(const char *format, ...);
 int _putchar(char c);
 typedef struct print_type
 {
         char *the_format_in_char;
-        void (*Funcp)();
+        int (*funcp)();
 } array;
 #endif /* print_type.h */
