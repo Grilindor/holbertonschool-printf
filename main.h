6ce278b81042decfdee0db2beab5a5861e53338c
Grilindor <marcor91@hotmail.fr>
2024-03-30 16:53:35 +0000

    correction to match the betty style

--- main.h
+++ main.h
@@ -12,9 +12,15 @@ int print_d_i(va_list chec_type);
 int print_modulo(va_list chec_type);
 int _printf(const char *format, ...);
 int _putchar(char c);
+
+/**
+ * struct print_type - link to fill where are the function
+ *@the_format_in_char: link with the format in _printf.c
+ *@funcp: link who call the funtion
+**/
 typedef struct print_type
 {
-        char *the_format_in_char;
-        int (*funcp)();
+	char *the_format_in_char;
+	int (*funcp)();
 } array;
 #endif /* print_type.h */
